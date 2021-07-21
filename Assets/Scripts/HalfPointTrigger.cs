d349ee6fb520eafb6c8b8132b2351bd57e3c8e5c
melissasoledade <melissa.gsoledade@gmail.com>
2021-07-21 18:41:19 -0300

    trigger-on

---
+++ Assets/Scripts/HalfPointTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTrigger : MonoBehaviour
+{
+    public GameObject LapComplete; 
+    public GameObject HalfLap;
+
+    void OnTriggerEnter(){        
+        LapComplete.SetActive(true); //ativar gatilho na pista do jogo
+        HalfLap.SetActive(false); 
+    }
+    
+}

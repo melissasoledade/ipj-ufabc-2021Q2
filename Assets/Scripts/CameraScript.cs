4745653df46363b6f8e87f07514e651e0ea63dc2
melissasoledade <melissa.gsoledade@gmail.com>
2021-07-21 16:41:35 -0300

    lap-time-panel

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject player;
+    public float playerX;
+    public float playerY;
+    public float playerZ;
+
+    // Start is called before the first frame update
+    /*void Start()
+    {
+        
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+     playerX = player.transform.eulerAngles.x;
+     playerY = player.transform.eulerAngles.y;
+     playerZ = player.transform.eulerAngles.z;
+
+     transform.eulerAngles = new Vector3(0, playerY, 0);
+
+    }
+}

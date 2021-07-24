eb34341aaf1cae9e2831e4101c8800af597e369c
Rafael Calpena Rodrigues <rafael.calpena@gmail.com>
2021-07-24 15:56:14 -0300

    fix: remove var inutilizada

--- Assets/BoostSpeed.cs
+++ Assets/BoostSpeed.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoostSpeed : MonoBehaviour {
-    /* Baseado na documentacao em https://docs.unity3d.com/ScriptReference/Rigidbody.AddRelativeForce.html */
-    public float thrust = 200000000000000;
     public GameObject car_root;
 
     void OnTriggerEnter() {
+        /* Define a velocidade para 50km/h */
+        int boostSpeed = 50;
         Vector3 v = car_root.GetComponent<Rigidbody>().velocity.normalized;
-        car_root.GetComponent<Rigidbody>().velocity = new Vector3(v.x * 50, v.y * 50, v.z * 50);
+        car_root.GetComponent<Rigidbody>().velocity = new Vector3(v.x * boostSpeed, v.y * boostSpeed, v.z * boostSpeed);
         Debug.Log("entered");
     }
 }
